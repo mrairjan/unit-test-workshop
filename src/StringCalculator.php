12dfdc956abe6de72aa24af91429d974c083ccd2
Mr. Air Jan <airidas.janavicius@gmail.com>
2018-03-22 07:35:05 +0200

    Update StringCalculator.php

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -1,19 +1,4 @@
 <?php
-/**
- * @copyright C UAB "NFQ Solutions" 2018
- *
- * This Software is the property of "Net Frequency"
- * and is protected by copyright law – it is NOT Freeware.
- *
- * Any unauthorized use of this software without a valid license key
- * is a violation of the license agreement and will be prosecuted by
- * civil and criminal law.
- *
- * Contact UAB "NFQ Solutions":
- * E-mail: info@nfq.lt
- * http://www.nfq.lt
- *
- */
 declare(strict_types=1);
 
 use Logger\LoggerInterface;
